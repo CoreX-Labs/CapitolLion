068f88d931bf0d835eda4959cff2ea180b591e54
George Olufemi <georgeoluolufemi@gmail.com>
2022-10-21 15:58:21 +0100

    Made Partners section for Desktop screen

--- CapitolLion_Marketplace/frontend/capitol-lion/src/pages/Home.jsx
+++ CapitolLion_Marketplace/frontend/capitol-lion/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Partners from './components/Partners';
 
 
 const Home = () => {
 	return (
 		<React.Fragment>
-			<div className="bg-[#121212] h-screen px-[24px] md:px-[70px] 2xl:px-[300px] text-white">
+			<div className="bg-[#121212] h-1000 px-[24px] md:px-[70px] 2xl:px-[300px] text-white">
 				<Hero />
+				<Partners />
 			</div>
 		</React.Fragment>
 	);
---
+++ CapitolLion_Marketplace/frontend/capitol-lion/src/pages/components/Partners.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../../App.css';
+
+const Partners = () => {
+	const partners = [
+		{
+			id: 1,
+			logo: '/CFinance.png',
+			companyname: 'calamus finance',
+		},
+		{
+			id: 2,
+			logo: '/NeoTech 1.png',
+			companyname: 'neotech',
+		},
+		{
+			id: 3,
+			logo: '/CML PNG.png',
+			companyname: 'coin market league',
+		},
+	];
+	return (
+		<React.Fragment>
+			<div className='pt-[40px] 2xl:pt-[172px] h-screen bg-[#121212]'>
+				<h1 className='text-center font-[500] text-[36px] leading-[80px] orbitron-light'>
+					Strategic Partners
+				</h1>
+				<div className='flex items-center justify-center pt-[49px]'>
+					<div className='w-[1021.77px] h-[210px] rounded-[20px] partner__div'>
+						<div className='flex items-center justify-center pt-[41px]'>
+							{partners.map((item, idx) => (
+								<div
+									key={ idx }
+									className='w-[917.77px] h-[128px] flex gap-[145px] justify-center'
+								>
+									<div>
+										<div className='flex items-center justify-center pb-[16.27px]'>
+											<img
+												className='w-[83.73px] h-[83.73px]'
+												src={ item.logo }
+												alt='Partners logo'
+											/>
+										</div>
+										<p className='uppercase orbitron-light font-[400] text-[24px] leading-[12px]'>
+											{ item.companyname }
+										</p>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Partners;
