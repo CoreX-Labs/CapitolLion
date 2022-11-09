cf0bfc2d4f9f20fef0a87f76bf59a8e8a1e0bc60
aridguy <rotimiariyo@gmail.com>
2022-11-09 18:44:44 +0100

    home page now responsive

--- CapitolLion_Marketplace/frontend/capitol-lion/src/pages/components/Partners.jsx
+++ CapitolLion_Marketplace/frontend/capitol-lion/src/pages/components/Partners.jsx
@@ -4,24 +4,24 @@ import '../../App.css';
 const Partners = () => {
   const partners = [
     {
-      id          : 1,
-      logo        : '/Purple LionX logo.png',
-      companyname : 'lionx'
+      id: 1,
+      logo: '/Purple LionX logo.png',
+      companyname: 'lionx'
     },
     {
-      id          : 2,
-      logo        : '/CFinance.png',
-      companyname : 'calamus finance'
+      id: 2,
+      logo: '/CFinance.png',
+      companyname: 'calamus finance'
     },
     {
-      id          : 3,
-      logo        : '/NeoTech 1.png',
-      companyname : 'neotech'
+      id: 3,
+      logo: '/NeoTech 1.png',
+      companyname: 'neotech'
     },
     {
-      id          : 4,
-      logo        : '/CML PNG.png',
-      companyname : 'coin market league'
+      id: 4,
+      logo: '/CML PNG.png',
+      companyname: 'coin market league'
     }
   ];
   return (
@@ -30,11 +30,11 @@ const Partners = () => {
         <h1 className='text-center font-[500] text-[24px] md:text-[36px] md:leading-[80px] orbitron-light'>
           Strategic Partners
         </h1>
-        <div className='flex items-center justify-center pt-[49px]'>
-          <div className='md:w-[1221.77px] w-[308px] md:h-[210px] rounded-[20px] partner__div'>
+        <div className='p-8 flex items-center justify-center pt-[49px]'>
+          <div className='w-full md:h-[210px] rounded-[20px] partner__div'>
             <div className='md:flex md:items-center md:justify-center pt-[41px] pb-[41px]'>
               {partners.map((item, idx) => (
-                <div key={idx} className='md:w-[917.77px] md:h-[128px] md:flex md:gap-[145px] md:justify-center'>
+                <div key={idx} className='w-full md:h-[128px] md:flex md:gap-[145px] md:justify-center'>
                   <div className="text-center pb-[40px]">
                     <div className='flex items-center justify-center pb-[16px] md:pb-[16.27px]'>
                       <img className='h-[83.73px]' src={item.logo} alt='Partners logo' />
