23de821e1405b4f87e4614a5744c11f6001a178b
aridguy <rotimiariyo@gmail.com>
2022-12-05 14:08:54 +0100

    creating and modifying the musicdao page

--- CapitolLion_Marketplace/frontend/capitol-lion/src/pages/MusicDAO.jsx
+++ CapitolLion_Marketplace/frontend/capitol-lion/src/pages/MusicDAO.jsx
@@ -1,46 +1,83 @@
-import React, { useState } from 'react';
-import axios from "axios";
+import React, { } from 'react';
+// import axios from "axios";
+import LogoMusicDAO from '../Assets/Logo.png'
+import './musicDAO.css'
+import LandImg from '../pages/MusicDaoAssets/Landimage.png'
+import LogoImagery from '../Assets/imagery.png'
+
 
 const MusicDAO = () => {
-	const [fileImg, setFileImg] = useState(null);
 
-	const sendFileToIPFS = async (e) => {
+	// const [fileImg, setFileImg] = useState(null);
 
-		if (fileImg) {
-						try {
+	// const sendFileToIPFS = async (e) => {
 
-										const formData = new FormData();
-										formData.append("file", fileImg);
+	// 	if (fileImg) {
+	// 		try {
 
-										const resFile = await axios({
-														method: "post",
-														url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-														data: formData,
-														headers: {
-																		'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
-																		'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
-																		"Content-Type": "multipart/form-data"
-														},
-										});
+	// 			const formData = new FormData();
+	// 			formData.append("file", fileImg);
 
-										const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
-							console.log(ImgHash); 
+	// 			const resFile = await axios({
+	// 				method: "post",
+	// 				url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+	// 				data: formData,
+	// 				headers: {
+	// 					'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
+	// 					'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
+	// 					"Content-Type": "multipart/form-data"
+	// 				},
+	// 			});
 
-						} catch (error) {
-										console.log("Error sending File to IPFS: ")
-										console.log(error)
-						}
-		}
-}
+	// 			const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+	// 			console.log(ImgHash);
+
+	// 		} catch (error) {
+	// 			console.log("Error sending File to IPFS: ")
+	// 			console.log(error)
+	// 		}
+	// 	}
+	// }
+
 
 	return (
 		<React.Fragment>
-			<div>
-			<form onSubmit={sendFileToIPFS}>
-			<input type="file" onChange={(e) =>setFileImg(e.target.files[0])} required />
-			<button type='submit' >Create</button>            
-			</form>
+
+			<div className='allBackground bg-gray-900'>
+				<div class="flex mb-4">
+					<div class="w-full bg-gray-900 h-12">
+						<div class="flex mt-6">
+							<div class="w-1/2 h-12">
+								<img width="90px" src={LogoMusicDAO} />
+							</div>
+							<div class="w-1/2 h-12">
+								<button className='supportBtn'>Support Capitol Lion</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className='md:container md:mx-auto max-width:768px columns-2 p-9 mt-9'>
+					<div>
+						<img className='sm:block imgs' src={LandImg} alt="music dao img" />
+					</div>
+					<div>
+						<img className='sm:block' src={LogoImagery} alt="music dao img" />
+						<p className='tx12'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							
+							<button className='bottonGradient'>Mint Now 1000 Trx</button>
+							<p className='tokentxt mt-8'>My Token IDs <br />
+							.........................</p>
+					</div>
+				</div>
+
 			</div>
+
 		</React.Fragment>
 	);
 };
